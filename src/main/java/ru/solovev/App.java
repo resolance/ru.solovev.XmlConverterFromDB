28f883136fd3f70bf629f8ba5a2f09d1f63fb802
resolance <kapusta>
2017-11-05 22:44:16 +0300

    3
    add

---
+++ src/main/java/ru/solovev/App.java
@@ -0,0 +1,32 @@
+package ru.solovev;
+
+import ru.solovev.ConvertXml.CreateXmlFromDb.MarshallerXmlFromDb;
+import ru.solovev.DbConnection.TableBuilder;
+
+public class App {
+    public static void main(String[] args) {
+
+        //TODO: Запрашиваем данные из бд и формируем XML сохраняем в систему
+        //TODO: с помощью XSLT преоразуем в другой XLM. сохраняем в систему
+        //TODO: парсим 2.xml и суммируем все значения полей филд - выводим в консоли
+        int numberOfInputRow = 0;
+        String pathToFirstXML = "";
+        try {
+            numberOfInputRow = Integer.parseInt(args[0]);
+            pathToFirstXML = args[1];
+        } catch (ClassFormatError e) {
+            e.printStackTrace();
+        }
+
+        try {
+            TableBuilder tableBuilder = new TableBuilder(numberOfInputRow);
+            tableBuilder.fillTable();
+            MarshallerXmlFromDb marshallerXmlFromDb = new MarshallerXmlFromDb(numberOfInputRow, pathToFirstXML);
+            marshallerXmlFromDb.getXmlFromDb();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("");
+    }
+}
